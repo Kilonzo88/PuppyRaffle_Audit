8d7bd7a0d42da760e32a4215b57acfd72e5e8987
Dennis <kilonzod19@gmail.com>
2026-01-12 16:43:18 +0300

    Pointed out some DOS vulnerability

--- src/PuppyRaffle.sol
+++ src/PuppyRaffle.sol
@@ -77,12 +77,14 @@ contract PuppyRaffle is ERC721, Ownable {
     /// @notice duplicate entrants are not allowed
     /// @param newPlayers the list of players to enter the raffle
     function enterRaffle(address[] memory newPlayers) public payable {
+        //q: were custom reverts a thing in 0.7.6 compiler version?
         require(msg.value == entranceFee * newPlayers.length, "PuppyRaffle: Must send enough to enter raffle");
         for (uint256 i = 0; i < newPlayers.length; i++) {
             players.push(newPlayers[i]);
         }
 
         // Check for duplicates
+        // @Audit: DoS attack possible here if too many players are added at once
         for (uint256 i = 0; i < players.length - 1; i++) {
             for (uint256 j = i + 1; j < players.length; j++) {
                 require(players[i] != players[j], "PuppyRaffle: Duplicate player");
