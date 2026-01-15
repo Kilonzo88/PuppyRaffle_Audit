9560daf9fd2466b4972283ad8126582b005f2b96
Dennis <kilonzod19@gmail.com>
2026-01-15 16:59:30 +0300

    Identied weak randomness that can be potentially manipulated by block validators

--- src/PuppyRaffle.sol
+++ src/PuppyRaffle.sol
@@ -132,7 +132,7 @@ contract PuppyRaffle is ERC721, Ownable {
         require(block.timestamp >= raffleStartTime + raffleDuration, "PuppyRaffle: Raffle not over");
         require(players.length >= 4, "PuppyRaffle: Need at least 4 players");
 
-        
+        //@Audit: Weak Randomness
         uint256 winnerIndex =
             uint256(keccak256(abi.encodePacked(msg.sender, block.timestamp, block.difficulty))) % players.length; //@audit: Weak Randomness
         address winner = players[winnerIndex];
